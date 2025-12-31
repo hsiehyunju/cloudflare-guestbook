3a164193fa1f443aeed57866a3b864fd2994241c
yunju <hsiehyunju@gmail.com>
2025-12-31 13:49:15 +0800

    [fix] resolve CORS policy blocking by adding cors middleware

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import { fromHono } from "chanfana";
 import { Hono } from "hono";
+import { cors } from "hono/cors";
 import { CommentCreate } from "./endpoints/commentCreate";
 import { CommentFetch } from "./endpoints/commentFetch";
 
 // Start a Hono app
 const app = new Hono<{ Bindings: Env }>();
+
+// Use default cors middleware
+app.use('/api/*', cors())
 
 // Setup OpenAPI registry
 const openapi = fromHono(app, {
